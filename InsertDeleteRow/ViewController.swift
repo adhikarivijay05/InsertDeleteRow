bb972bb928b1dbe36779366dc5d25dad720d367d
adhikarivijay05 <adhikari.vijay05@example.comgit config --global user.email adhikari.vijay05@example.com>
2017-07-01 12:50:11 +0530

    insertdeleteRow

--- InsertDeleteRow/ViewController.swift
+++ InsertDeleteRow/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource{
 
+    @IBOutlet weak var ContentTable: UITableView!
+   
+    
+    @IBOutlet weak var txtView: UITextField!
+    
+    //
+    var dataArray = ["swift is awesome language", "iPhone 7 is nice", "we love apple"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ContentTable.estimatedRowHeight = 78
+        ContentTable.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  =  ContentTable.dequeueReusableCell(withIdentifier: "cell") as! dataCell
+        cell.title.text = dataArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            dataArray.remove(at: indexPath.row)
+            ContentTable.beginUpdates()
+            ContentTable.deleteRows(at: [indexPath], with: .automatic)
+            ContentTable.endUpdates()
+        }
+    }
+    
+    @IBAction func addRow(_ sender: UIButton) {
+        
+        if (txtView.text?.isEmpty)! {
+            return
+        }
+        dataArray.append(txtView.text!)
+        
+        let indexPath = IndexPath(row: dataArray.count - 1 , section: 0)
+        ContentTable.beginUpdates()
+        ContentTable.insertRows(at: [indexPath], with: .automatic)
+        ContentTable.endUpdates()
+        txtView.text = ""
+        view.endEditing(true)
+        
+        
+    }
+    
+    
+    
+    
 }
 
---
+++ InsertDeleteRow/dataCell.swift
@@ -0,0 +1,25 @@
+//
+//  dataCell.swift
+//  InsertDeleteRow
+//
+//  Created by Vijay Adhikari on 01/07/2017.
+//  Copyright © 2017 Vijay Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class dataCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
